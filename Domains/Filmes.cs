ea91cd3d085e2e4a4d116948cb07513276b2081a
Isaac Borges Oliveira <isaac.b.oliveira@aluno.senai.br>
2025-02-20 08:57:09 -0300

    subindo o ex api filmes

---
+++ Domains/Filmes.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_filmes_senai.Domains
+{
+    [Table("Filme")]
+    public class Filmes
+    {
+        [Key]
+        public Guid IdFilmes { get; set; }
+
+        [Column(TypeName = "VARCHAR (50)")]
+        [Required(ErrorMessage = "O titulo do filme é obrigatorio!")]
+        public string? Titulo { get; set; }
+     /// <summary>
+     /// Referencia da tabela de Genero
+     /// </summary>
+        public Guid IdGenero { get; set; }
+        [ForeignKey("IdGenero")]
+        public Genero? Genero { get; set; } 
+    }
+}
---
+++ Migrations/20250218185600_Db_filmes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_filmes_senai.Migrations
+{
+    /// <inheritdoc />
+    public partial class Db_filmes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genero",
+                columns: table => new
+                {
+                    IdGenero = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "VARCHAR(30)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genero", x => x.IdGenero);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genero");
+        }
+    }
+}
